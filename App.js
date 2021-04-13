6461db277289b7520b6c7f4e6716aeec62258574
Zain Mir <zainnaeemk10@gmail.com>
2021-04-13 18:32:19 +0500

    setting player states

--- App.js
+++ App.js
@@ -1,53 +1,57 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, Pressable } from "react-native";
 
 export default function App() {
+  const [value, setvalue] = useState();
+  const [player1, setplayer1] = useState();
+  const [player2, setplayer2] = useState();
   return (
     <View style={styles.container}>
       <Text>Tic Tac Toe</Text>
       <View style={styles.button}>
         <Pressable>
-          <Button  title="O" color="orange" />
+          <Button title="O" color="grey" />
         </Pressable>
         <Pressable>
-          <Button title="O" color="orange" />
-        </Pressable>
-
-        <Pressable>
-          <Button title="O" color="orange" />
-        </Pressable>
-
-        <StatusBar style="auto" />
-      </View>
-      <View style={styles.button}>
-        <Pressable>
-          <Button title="O" color="orange" />
-        </Pressable>
-        <Pressable>
-          <Button title="O" color="orange" />
+          <Button title="x" color="orange" />
         </Pressable>
 
         <Pressable>
-          <Button title="O" color="orange" />
+          <Button title="O" color="grey" />
         </Pressable>
 
         <StatusBar style="auto" />
       </View>
       <View style={styles.button}>
         <Pressable>
-          <Button title="O" color="orange" />
+          <Button title="x" color="orange" />
         </Pressable>
         <Pressable>
-          <Button title="O" color="orange" />
+          <Button title="O" color="grey" />
         </Pressable>
 
         <Pressable>
-          <Button title="O" color="orange" />
+          <Button title="x" color="orange" />
         </Pressable>
 
         <StatusBar style="auto" />
       </View>
+      <View style={styles.button}>
+        <Pressable>
+          <Button title="O" color="grey" />
+        </Pressable>
+        <Pressable>
+          <Button title="x" color="orange" />
+        </Pressable>
+
+        <Pressable>
+          <Button title="x" color="orange" />
+        </Pressable>
+
+        <StatusBar style="auto" />
+      </View>
+      <Text> player 1:0</Text>
     </View>
   );
 }
@@ -60,11 +64,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  button:{
-    margin:10,
-    
-    padding:10,
-    flexDirection:'row'
+  button: {
+    margin: 10,
+
+    padding: 10,
+    flexDirection: "row",
   },
 });
-
